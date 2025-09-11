e43802027ab5580ad13a30e96c8af0647976cb16
JoseLeonardoPerez <joseleonardoperez4@gmail.com>
2025-09-11 15:49:54 -0300

    Backend para trabajar con React-vite y Angular

--- src/main/java/com/joseleonardo/springboot/backend/springboot_backend/controllers/ProdcutController.java
+++ src/main/java/com/joseleonardo/springboot/backend/springboot_backend/controllers/ProdcutController.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin({"http://localhost:5173", "http://localhost:4200"})
+@RequestMapping("/products")
 public class ProdcutController {
     final private ProductService service;
 
